1672729f76390f41262515a513d396b6c7a8791c
Mindaugas Pelionis <mindaugas.pelionis@gmail.com>
2015-09-21 15:29:15 +0300

    Added: .vimrc.

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@
 # Set up symlinks in home directory to dotfiles
 ln -svi .dotfiles/.bash_profile ~/.bash_profile
 ln -svi .dotfiles/.bashrc ~/.bashrc
+ln -svi .dotfiles/.vimrc ~/.vimrc
 
 # Set up Git
 ln -svi .dotfiles/.gitignore_global ~/.gitignore_global
