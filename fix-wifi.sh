62ad869664e16b9a027fa4b2dbc08bc9e20dbbe2
Mindaugas Pelionis <mindaugas.pelionis@gmail.com>
2015-10-26 10:07:37 +0200

    Added: script to reload OS X WiFi.

---
+++ fix-wifi.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+#
+
+# Fixing OS X WiFi issues.
+# Reference: http://apple.stackexchange.com/questions/195359/wifi-keeps-dropping-macbook-pro-13in-retina-early-2015
+
+echo "Disable WiFi, and press Enter to continue or Ctrl+C to cancel."
+read foo
+
+prefix="${HOME}/wifi_backup/"
+timestamp=`date "+%Y-%m-%d_%H%M%S"`
+dirname=$prefix$timestamp
+
+mkdir -pv $dirname
+mv \
+	/Library/Preferences/SystemConfiguration/com.apple.airport.preferences.plist \
+	/Library/Preferences/SystemConfiguration/NetworkInterfaces.plist \
+	/Library/Preferences/SystemConfiguration/preferences.plist \
+	$dirname
+
+echo "The system will restart now. Re-enable WiFi when it does."
+shutdown -r now
