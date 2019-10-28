51c90834a0e6cbbb741ce5ec10f22681c9e43dc5
Mindaugas Pelionis <mindaugas.pelionis@gmail.com>
2019-10-28 16:21:15 +0200

    Add: Python 3.7 to path.

--- common_env.sh
+++ common_env.sh
@@ -4,7 +4,7 @@
 
 # Environment variables.
 # export PATH="~/.composer/vendor/bin:~/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:~/.rbenv/versions/`cat ~/.rbenv/version`/bin:$PATH"
-export PATH="~/.composer/vendor/bin:~/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:`brew --prefix qt@5.5`/bin:$PATH"
+export PATH="~/.composer/vendor/bin:~/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:`brew --prefix qt@5.5`/bin:~/Library/Python/3.7/bin:$PATH"
 export PATH="~/.yarn/bin:~/.config/yarn/global/node_modules/.bin:$PATH"
 export PATH="/usr/local/opt/libxml2/bin:$PATH"
 export LANG="en_US.UTF-8"
