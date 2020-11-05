5cdf5b9850432b19f3844b9ef411394e99bcd1eb
Mindaugas Pelionis <mindaugas.pelionis@vinted.com>
2020-11-05 13:43:36 +0200

    Clean up: remove nvm configuration.

--- common_env.sh
+++ common_env.sh
@@ -19,11 +19,6 @@ mcd() {
 # rbenv
 if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
 
-# nvm
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
 # secrets
 if [ -f ~/.dotfiles/secrets.sh ]; then
   source ~/.dotfiles/secrets.sh
