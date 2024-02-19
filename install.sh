587d95b82e5809bd341836fd75bb446a40eb5f11
Mindaugas Pelionis <mindaugas.pelionis@gmail.com>
2024-02-19 22:44:47 +0200

    Add: backup the configuration files if they exist before symlinking the configuration in .dotfiles.

--- install.sh
+++ install.sh
@@ -8,6 +8,10 @@ CONFIG_FILES=".bash_profile .bashrc .zshrc .vimrc .gitignore_global"
 
 # Backup existing configuration files and create symlinks
 for file in $CONFIG_FILES; do
-  ln -svi "$DOTFILES_DIR/$file" "$HOME/$file"
+  if [ -f "$HOME/$file" ]; then
+    mv "$HOME/$file" "$HOME/$file.backup"
+    echo "Backed up $HOME/$file to $HOME/$file.backup"
+  fi
+  ln -sf "$DOTFILES_DIR/$file" "$HOME/$file"
   echo "Created symlink for $HOME/$file"
 done
