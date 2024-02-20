a50f8f30bb68d2251d73da112bb6613348670754
Mindaugas Pelionis <mindaugas.pelionis@gmail.com>
2024-02-20 18:06:23 +0200

    Changed: simplify backup-and-symlink operation in install.sh

--- install.sh
+++ install.sh
@@ -8,10 +8,5 @@ CONFIG_FILES=".bash_profile .bashrc .zprofile .zshrc .vimrc .gitignore_global"
 
 # Backup existing configuration files and create symlinks
 for file in $CONFIG_FILES; do
-  if [ -f "$HOME/$file" ]; then
-    mv "$HOME/$file" "$HOME/$file.backup"
-    echo "Backed up $HOME/$file to $HOME/$file.backup"
-  fi
-  ln -sf "$DOTFILES_DIR/$file" "$HOME/$file"
-  echo "Created symlink for $HOME/$file"
+  ln -s --backup=numbered "$DOTFILES_DIR/$file" "$HOME/$file"
 done
